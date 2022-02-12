c4f5851c0f07c264fc9d85de201b090e39f8322a
Taha Keen <76200293+pot8osince98@users.noreply.github.com>
2022-02-12 15:09:57 -0500

    Create DB0201EN-Week3-1-3-Subqueries-and-Nested-SELECTs.sql

---
+++ DB0201EN-Week3-1-3-Subqueries-and-Nested-SELECTs.sql
@@ -0,0 +1,23 @@
+/* Execute a failing query (i.e. one which gives an error) to retrieve all 
+employees records whose salary is lower than the average salary. */
+
+select * from EMPLOYEES where SALARY < AVG(SALARY);
+
+/* Execute a working query using a sub-select to retrieve all 
+employees records whose salary is lower than the average salary. */
+
+select * from EMPLOYEES where SALARY < (select avg(SALARY) from EMPLOYEES);
+
+/* Execute a failing query (i.e. one which gives an error) to 
+retrieve all employees records with EMP_ID, SALARY and maximum 
+salary as MAX_SALARY in every row. */
+
+select EMP_ID, SALARY, (select max(SALARY) from EMPLOYEES) as MAX_SALARY 
+from EMPLOYEES;
+
+/* Execute a Table Expression for the EMPLOYEES table that excludes 
+columns with sensitive employee data (i.e. does not include 
+columns: SSN, B_DATE, SEX, ADDRESS, SALARY). */
+
+select * from ( select EMP_ID, F_NAME, L_NAME, DEP_ID from employees) 
+AS EMP4ALL;
